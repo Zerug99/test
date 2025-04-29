faa6df544c24016ba4d2c4649639cde450cf8b37
unknown <patryk-99@hotmail.com>
2025-04-29 09:04:05 +0200

    variabler

---
+++ namnAlder.ps1
@@ -0,0 +1,14 @@
+# Skapa två variabler och lägg ihop dem fllr att sedan skriva ut 
+
+
+# variabler för namn
+$namn = "Alice"
+
+
+# Varaibler för ålder 
+$alder = 25
+
+
+# print varabiler 
+Write-Host "hej $namn, du är $alder år gammal"
+
