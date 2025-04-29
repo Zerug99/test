8baa43acb6785dc4a982fc8d25918272307b3306
unknown <patryk-99@hotmail.com>
2025-04-29 09:38:21 +0200

    if else övning

---
+++ kontrollAvTal.ps1
@@ -0,0 +1,10 @@
+# Kolla om ett tal är större än 5, if statment 
+
+#tal = 10 
+
+if ($tal -gt 5) {
+    Write-Host "Talet är större än 5"
+}
+else {
+    Write-Host "Talet är 5 eller mindre"
+}
--- namnAlder.ps1
+++ namnAlder.ps1
@@ -1,14 +1,10 @@
 # Skapa två variabler och lägg ihop dem fllr att sedan skriva ut 
 
-
 # variabler för namn
 $namn = "Alice"
-
 
 # Varaibler för ålder 
 $alder = 25
 
-
 # print varabiler 
 Write-Host "hej $namn, du är $alder år gammal"
-
